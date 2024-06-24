ea8956926894e2f3b353cfda01a8206ac652aa12
kalidasskrishnan <kalidass.k@ampcome.com>
2024-06-24 21:13:20 +0530

    dev_init

--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -1,40 +1,32 @@
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const navigation = [
-  { name: "about", href: "#about", current: true },
-  { name: "menu", href: "/menu", current: false },
-  { name: "contact", href: "#content", current: false },
-  { name: "reserve", href: "#reserve", current: false },
+  { name: "About", href: "#about", current: true },
+  { name: "Menu", href: "/menu", current: false },
+  { name: "Contact", href: "#content", current: false },
+  { name: "Reserve", href: "#reserve", current: false },
 ];
 
 const navigation_start = [
-  { name: "about", href: "#about", current: true },
-  { name: "menu", href: "/menu", current: false },
+  { name: "Gift cards", href: "https://www.talech.com/biz/ordering/143524/MERCURY-BAR-LOUNGE-KINGSTON-ON#/gift_cards", current: false },
+  { name: "About", href: "#about", current: true },
+  { name: "Menu", href: "/menu", current: false },
 ];
 const navigation_end = [
-  { name: "contact", href: "#content", current: false },
-  { name: "reserve", href: "#reserve", current: false },
+  { name: "Contact", href: "#content", current: false },
+  { name: "Reserve", href: "#reserve", current: false },
+  { name: "Order online", href: "https://www.talech.com/biz/ordering/143524/MERCURY-BAR-LOUNGE-KINGSTON-ON", current: false},
 ];
 
 export default function NaVBar() {
-  useEffect(() => {
-    // Disable scrolling
-    document.body.style.overflow = 'hidden';
-    return () => {
-      // Enable scrolling when the component unmounts
-      document.body.style.overflow = 'auto';
-    };
-  }, []);
   return (
-    <div className="relative min-h-screen bg-cover bg-center" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
     <Disclosure as="nav" className="bg-[#333333]">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-20 items-center justify-between">
+          <div className=" px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-24 w-full items-center justify-around">
               <div className="relative inset-y-0 left-0 flex justify-end sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative flex w-full items-end justify-end rounded-md p-2 text-gray-400 hover:text-white">
@@ -54,13 +46,13 @@ export default function NaVBar() {
                 </Disclosure.Button>
               </div>
               <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4 items-center justify-evenly sm:items-stretch sm:justify-start lg:w-1/2">
+                <div className="flex gap-x-14 sm:items-stretch sm:justify-start">
                   {navigation_start.map((item) =>
                     item.href[0] === "#" ? (
                       <a
                         key={item.name}
                         href={`/${item.href}`}
-                        className="hover:underline hover:decoration-solid hover:decoration-[1.5px] rounded-md px-3 py-2 text-sm font-medium  text-white"
+                        className="hover:underline hover:decoration-solid hover:decoration-[1.5px] rounded-md px-3 py-2 text-base font-medium  text-white"
                       >
                         {item.name}
                       </a>
@@ -68,7 +60,7 @@ export default function NaVBar() {
                       <Link
                         key={item.name}
                         to={item.href}
-                        className="hover:underline hover:decoration-solid hover:decoration-[1.5px] rounded-md px-3 py-2 text-sm font-medium  text-white"
+                        className="hover:underline hover:decoration-solid hover:decoration-[1.5px] rounded-md px-3 py-2 text-base font-medium  text-white"
                       >
                         {item.name}
                       </Link>
@@ -82,17 +74,27 @@ export default function NaVBar() {
               </div>
 
               <div className="hidden sm:ml-6 sm:block">
-                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-end">
-                  <div className="flex space-x-4">
-                    {navigation_end.map((item) => (
+                <div className="flex flex-1 gap-x-16 sm:items-stretch sm:justify-end">
+                  <div className="flex gap-x-16">
+                  {navigation_end.map((item) =>
+                    item.href[0] === "#" ? (
                       <a
                         key={item.name}
                         href={`/${item.href}`}
-                        className="hover:underline hover:decoration-solid hover:decoration-[1.5px] rounded-md px-3 py-2 text-sm font-medium  text-white"
+                        className="hover:underline hover:decoration-solid hover:decoration-[1.5px] rounded-md px-3 py-2 text-base font-medium  text-white"
                       >
                         {item.name}
                       </a>
-                    ))}
+                    ) : (
+                      <Link
+                        key={item.name}
+                        to={item.href}
+                        className="hover:underline hover:decoration-solid hover:decoration-[1.5px] rounded-md px-3 py-2 text-base font-medium  text-white"
+                      >
+                        {item.name}
+                      </Link>
+                    )
+                  )}
                   </div>
                 </div>
               </div>
@@ -115,18 +117,5 @@ export default function NaVBar() {
         </>
       )}
     </Disclosure>
-
-
-      <div className="absolute inset-0 flex items-center justify-center z-10">
-        <div className="backdrop-opacity-10 backdrop-invert bg-white/10 backdrop-blur-sm flex border-2 border-black text-white p-8 rounded-3xl">
-          <div className="text-4xl font-bold">
-            Opening Soon
-          </div>
-          <div className="flex items-baseline mt-4 pl-3">
-            <div className='loader'></div>
-          </div>
-        </div>
-      </div>
-    </div>
   );
 }
